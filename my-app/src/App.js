5ccaf187f9a4bfe35e1eecbabdce29ecfb4a96e6
unknown <mikael.jafari@gmail.com>
2022-02-09 18:45:25 +0100

    feat: implement data gathering from server (#1)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,65 +2,45 @@ import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
 
-var muldata = require('./sampledata.json');
-var sindata = require('./singledata.json');
-
 class App extends Component {
   state = {
     rows: [],
-    lmao: "asdkj"
+    searchedId: [],
+    searchVal: ''
   };
   
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount(){
-    fetch("http://nichujie.xyz:8080/history")
+    fetch("/history")
     .then(response => response.json())
-    .then(data => console.log(data));
-    console.log("alskdjalsdj");
+    .then(data => this.setState({
+      rows:data 
+    }));
+    
   }
 
+  handleChange(event) {
+    this.setState({searchVal: event.target.value});  
+  }
+  
   handleClick(){
-    this.setState({
-      rows: sindata
-    }, () => {console.log(this.state.rows)});
+    fetch("/history/"+this.state.searchVal)
+    .then(response => response.json())
+    .then(data => this.setState({
+      searchedId:[data]
+    }));
     
   }
   render() {
     return (
       <div className="App">
-        <div className="sides" id='left'>
-          <div>
+        <div className="sides" id="left">
+        <div>
             <p>All builds</p>
-          </div>
-          <table id="lefttable">
-            <thead>
-              <tr>
-                <th>uid</th>
-                <th className="lefthash">commit_hash</th>
-                <th>content</th>
-                <th>timestamp</th>  
-              </tr>
-            </thead>
-            <tbody>
-              {muldata.reverse().map(element =>(
-                <tr>
-                  <td>{element.uid}</td>
-                  <td>{element.commitId}</td>
-                  <td>{element.content}</td>
-                  <td>{element.timestamp}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="sides" id="right">
-          <div>              
-            <button onClick={this.handleClick}>
-              Get build
-            </button>
           </div>
           <table>
             <thead>
@@ -73,6 +53,34 @@ class App extends Component {
             </thead>
             <tbody>
               {this.state.rows.map(element =>(
+                <tr>
+                  <td>{element.uid}</td>
+                  <td>{element.commitId}</td>
+                  <td>{element.content}</td>
+                  <td>{element.timestamp}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="sides" id='right'>
+          <div>              
+            <button onClick={this.handleClick}>
+              Get build
+            </button>
+            <input value={this.state.searchVal} onChange={this.handleChange} type="text"/>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>uid</th>
+                <th>commit_hash</th>
+                <th>content</th>
+                <th>timestamp</th>  
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.searchedId.map(element =>(
                 <tr>
                   <td>{element.uid}</td>
                   <td>{element.commitId}</td>
